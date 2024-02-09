563fdcddf9559aa5f0b62610d090e33b86dbf314
FazillD <m.fazildemircan@gmail.com>
2024-02-09 15:53:21 +0300

    Initial Commit

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var studentName = "Ahmet"
+var studentAge = 23
+var studentHeight = 1.87
+
+print(studentName)
+print(studentAge)
+print(studentHeight)
+
+var productID:Int = 3416
+var productName:String = "Watch"
+var productStock:Int = 100
+var productPrice:Double = 149.99
+
+print("Product ID : \(productID)")
+print("Product Name : \(productName)")
+print("Product Stock : \(productStock)")
+print("Product Price : \(productPrice)")
